80b0d159ebf2338839684bf82e6c11cda043180b
William Zang <chengwei.zang.1985@gmail.com>
2018-07-10 15:31:35 +0800

    <feat>(<location>) : <- mapEach: use macro instead of ugly code>

--- EasyReact/Classes/Core/Private/EZRNode+MapEach.m
+++ EasyReact/Classes/Core/Private/EZRNode+MapEach.m
@@ -31,29 +31,13 @@
         if (![tuple isKindOfClass:[EZTupleBase class]]) {
             EZR_THROW(EZRNodeExceptionName, EZRExceptionReason_MapEachNextValueNotTuple, nil)
         }
-        if (!block) {
-            return nil;
-        }
+        if (!block) { return nil; }
+#define EZR_tupleGet(_index_)       fakeTuple16. EZ_ORDINAL_AT(_index_)
+#define EZR_mapBlock(_index_)       case _index_: return block(EZ_FOR_COMMA(_index_, EZR_tupleGet));
+        EZTuple16 *fakeTuple16 = (EZTuple16 *)tuple;
         switch (tuple.count) {
-            case 0: return block();
-            case 1: return block(tuple[0]);
-            case 2: return block(tuple[0], tuple[1]);
-            case 3: return block(tuple[0], tuple[1], tuple[2]);
-            case 4: return block(tuple[0], tuple[1], tuple[2], tuple[3]);
-            case 5: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4]);
-            case 6: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5]);
-            case 7: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5], tuple[6]);
-            case 8: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5], tuple[6], tuple[7]);
-            case 9: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5], tuple[6], tuple[7], tuple[8]);
-            case 10: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5], tuple[6], tuple[7], tuple[8], tuple[9]);
-            case 11: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5], tuple[6], tuple[7], tuple[8], tuple[9], tuple[10]);
-            case 12: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5], tuple[6], tuple[7], tuple[8], tuple[9], tuple[10], tuple[11]);
-            case 13: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5], tuple[6], tuple[7], tuple[8], tuple[9], tuple[10], tuple[11], tuple[12]);
-            case 14: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5], tuple[6], tuple[7], tuple[8], tuple[9], tuple[10], tuple[11], tuple[12], tuple[13]);
-            case 15: return block(tuple[0], tuple[1], tuple[2], tuple[3], tuple[4], tuple[5], tuple[6], tuple[7], tuple[8], tuple[9], tuple[10], tuple[11], tuple[12], tuple[13], tuple[14]);
-                
-            default:
-                return nil;
+            EZ_FOR(16, EZR_mapBlock, )
+            default: return nil;
         }
     }];
 }
