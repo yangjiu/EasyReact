19610b48ecb769a7ea6941275c6c3f5ec5f78584
jundian <jundianapple@icloud.com>
2018-08-19 16:40:21 +0800

    fix(location) : keep code style consistent.

--- EasyReact/Classes/Core/EZRMetaMacros.h
+++ EasyReact/Classes/Core/EZRMetaMacros.h
@@ -16,27 +16,27 @@
 
 #import <EasyFoundation/EasyFoundation.h>
 
-#define _EZRCombine(...)  ((EZ_CONCAT(EZRMapEachNode, EZ_ARG_COUNT(__VA_ARGS__)) *)[EZRNode combine:@[__VA_ARGS__]])
+#define _EZRCombine(...)                    ((EZ_CONCAT(EZRMapEachNode, EZ_ARG_COUNT(__VA_ARGS__)) *)[EZRNode combine:@[__VA_ARGS__]])
 
-#define _EZRZip(...)  ((EZ_CONCAT(EZRMapEachNode, EZ_ARG_COUNT(__VA_ARGS__)) *)[EZRNode zip:@[__VA_ARGS__]])
+#define _EZRZip(...)                        ((EZ_CONCAT(EZRMapEachNode, EZ_ARG_COUNT(__VA_ARGS__)) *)[EZRNode zip:@[__VA_ARGS__]])
 
-#define _EZR_BLOCK_ARG(index)         EZ_CONCAT(arg, index)
-#define _EZR_BLOCK_ARG_DEF(index)     id EZ_CONCAT(arg, index)
+#define _EZR_BLOCK_ARG(index)               EZ_CONCAT(arg, index)
+#define _EZR_BLOCK_ARG_DEF(index)           id EZ_CONCAT(arg, index)
 
-#define _EZR_BLOCK_DEF(N)                id (^)(EZ_FOR_COMMA(N, _EZR_BLOCK_ARG_DEF))
+#define _EZR_BLOCK_DEF(N)                   id (^)(EZ_FOR_COMMA(N, _EZR_BLOCK_ARG_DEF))
 
 #define _EZR_DEF_REDUCE_VALUE(N)                                 \
-@interface EZ_CONCAT(EZRMapEachNode, N) : EZRNode                  \
-- (EZRNode *)mapEach:(_EZR_BLOCK_DEF(N))block;                    \
+@interface EZ_CONCAT(EZRMapEachNode, N) : EZRNode                \
+- (EZRNode *)mapEach:(_EZR_BLOCK_DEF(N))block;                   \
 @end
 
-#define _EZR_DEF_REDUCE_VALUE_ITER(index)             _EZR_DEF_REDUCE_VALUE(EZ_INC(index))
+#define _EZR_DEF_REDUCE_VALUE_ITER(index)    _EZR_DEF_REDUCE_VALUE(EZ_INC(index))
 
-#define EZR_MapEachFakeInterfaceDef(N)           EZ_FOR(N, _EZR_DEF_REDUCE_VALUE_ITER, ;)
+#define EZR_MapEachFakeInterfaceDef(N)       EZ_FOR(N, _EZR_DEF_REDUCE_VALUE_ITER, ;)
 
-#define _EZR_KeyPath(OBJ, PATH)  (((void)(NO && ((void)OBJ.PATH, NO)), @# PATH))
+#define _EZR_KeyPath(OBJ, PATH)              (((void)(NO && ((void)OBJ.PATH, NO)), @# PATH))
 
-#define _EZR_PATH(TARGET, KEYPATH)               TARGET.ezr_path[_EZR_KeyPath(TARGET, KEYPATH)]
+#define _EZR_PATH(TARGET, KEYPATH)           TARGET.ezr_path[_EZR_KeyPath(TARGET, KEYPATH)]
 
 
 #define EZR_THROW(NAME, REASON, INFO)                            \
@@ -49,9 +49,9 @@
 
 #define ezr_strongify(...)                                       \
     ezr_keywordify                                               \
-    _Pragma("clang diagnostic push")                            \
-    _Pragma("clang diagnostic ignored \"-Wshadow\"")            \
-    EZ_FOR_EACH(ezr_strongify_, , __VA_ARGS__)                     \
+    _Pragma("clang diagnostic push")                             \
+    _Pragma("clang diagnostic ignored \"-Wshadow\"")             \
+    EZ_FOR_EACH(ezr_strongify_, , __VA_ARGS__)                   \
     _Pragma("clang diagnostic pop")
 
 
